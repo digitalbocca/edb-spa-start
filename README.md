1b033b38f8e447ebccd5616d42e6741b2a9b0aa0
Gabriel Bertola Bocca <digitalbocca@gmail.com>
2019-10-22 15:14:21 -0300

    Alteração no layout da página principal

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## edb-spa-start
 
-### v0.1.0
+### v0.3.0
 
 > Descrição
 
--- src/components/app.vue
+++ src/components/app.vue
@@ -1,9 +1,19 @@
 <template lang="pug">
-  .container-fluid#app
-    .row
-      .col-sm-12
-        h1 EDB SPA Start
-    router-view/
+  .container-fluid.app#app
+    .container-fluid.title-box
+      .row.py-4
+        .col-sm-12.text-center
+          h1.font-weight-bold EDB SPA Start
+    .container
+      router-view
+    .container-fluid.title-box
+      .row.py-4
+        .col.text-center
+          p.footer-links Imagem de 
+            a(href="https://pixabay.com/pt/users/jplenio-7645255/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=4563464") Johannes Plenio 
+            | por 
+            a(href="https://pixabay.com/pt/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=4563464") Pixabay
+          p 2019 - Estúdio Digital Bocca
 </template>
 
 <script>
@@ -20,3 +30,15 @@ export default {
   }
 }
 </script>
+
+<style lang="sass">
+
+  .footer-links > a, a:hover, a:active, a:visited
+    color: #fff
+    font-weight: bold
+  
+  .footer-links > a:hover
+    cursor: pointer
+    text-decorarion: underline
+
+</style>
